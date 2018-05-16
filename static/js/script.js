7171f0ae916162ef53e70257562f97eebe35fa9e
oddbary <sauki2@icloud.com>
2018-05-16 16:49:05 +0900

    app.pyの消失
    app.pyの消失の憂鬱

--- static/js/script.js
+++ static/js/script.js
@@ -17,16 +17,29 @@ $(function(){
     }
   })
 
+  var blockList = '';
+
   //ブロックがクリックされた場合
   $('.sec-block-set__top td').on('click',function(){
-    if($('.js-edit').hasClass('active')){ //編集モードのとき
+    if($('.js-edit').hasClass('active') && !($(this).hasClass('on')) ){ //編集モードかつ該当のブロックがonになっていない
       var color = $('.sec-block-set__color-choose').find('.active').attr('id');
       $(this).removeClass();
       $(this).addClass(color);
       $(this).addClass('on');
-      console.log('あれ？？？？？？？？');
+      var blockId = $(this).attr('id');
+      //カラーブロックのリスト
+      blockList += blockId + ',';
+      console.log(blockList);
+      // セッションの格納
+      window.sessionStorage.setItem('blockList',blockList);
     }else if($('.js-delete').hasClass('active')){ //削除モードのとき
       $(this).removeClass();
+      var blockId = $(this).attr('id');
+      //カラーブロックのリスト削除
+      blockList = blockList.replace( blockId+',' , '' );
+      console.log(blockList);
+      // セッションの格納
+      window.sessionStorage.setItem('blockList',blockList);
     }
   });
 
